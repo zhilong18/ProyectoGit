e014bc3855c46430c0764866eef43c993ca9eec1
zhilong18 <126709677+zhilong18@users.noreply.github.com>
2023-09-19 20:42:48 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ProyectoFullStack1 {
+}
---
+++ src/paquete1_git/Clase1.java
@@ -0,0 +1,10 @@
+package paquete1_git;
+
+public class Clase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
